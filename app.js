10c733d1d6c07a7800c10d9c98b83ef9bd277d4c
vinamratiwari2009-beep <vinamratiwari2009@gmail.com>
2026-06-30 13:21:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path = require('path');
 // ============================================
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // ============================================
 // SECTION 3: MIDDLEWARE
